3a7305cf869b2271133bbbaef2132ba48dfbe9c8
DmitriyGalyanov <dimagalyanov@gmail.com>
2020-11-24 19:32:04 +0300

    add SecondaryMenuScreen

---
+++ src/screens/SecondaryMenuScreen/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import {
+	StyleSheet,
+	ImageBackground,
+	Image,
+	View,
+} from 'react-native';
+
+import {useNavigation} from '@react-navigation/native';
+
+import SimpleButton from 'components/SimpleButton';
+
+import background from 'images/mainBackground.jpg';
+import coin from 'images/coin.png';
+
+
+export default function SecondaryMenuScreen() {
+	const navigation = useNavigation();
+
+	return (
+		<ImageBackground
+			source={background}
+			style={styles.wrap}
+		>
+			<Image
+				source={coin}
+				style={styles.coinImage}
+			/>
+			<View style={styles.buttonsWrap}>
+				<View style={styles.buttonWrap}>
+					<SimpleButton
+						title='Меню'
+						onPress={() => navigation.push('MainMenu')}
+					/>
+				</View>
+				<View style={styles.buttonWrap}>
+					<SimpleButton
+						title='Рестарт'
+						onPress={() => navigation.push('Game')}
+					/>
+				</View>
+			</View>
+		</ImageBackground>
+	)
+}
+
+const styles = StyleSheet.create({
+	wrap: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'space-evenly',
+	},
+
+	coinImage: {
+		height: 120,
+		width: 120,
+	},
+
+	buttonsWrap: {
+		// height: 138,
+		// justifyContent: 'space-between',
+	},
+
+	buttonWrap: {
+		marginBottom: 32,
+	},
+});
